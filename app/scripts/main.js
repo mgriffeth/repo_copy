e55fb28a55b72aa348d7a1c1db8ae02a715938c2
Matthew Griffeth <matthewgriffeth@Matthews-MacBook-Pro.local>
2014-10-09 16:31:16 -0400

    working on sidebar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('The Iron Yard Rocks');
+var repos_url = 'https://api.github.com/users/mgriffeth/repos',
+    user_url = 'https://api.github.com/users/mgriffeth';
+
+
+var repo_template_string =$('#repo_template').html();
+
+var rendered_repo_template = _.template(repo_template_string);
+
+var repos_api = $.getJSON(repos_url).done(function(x){
+  //console.log(x);
+  x.forEach(function(repo){
+    //console.log(repo.name);
+    $('.repositories').append( rendered_repo_template (repo));
+  })
+});
+
+var user_api = $.getJSON(user_url).done(function(user){
+  console.log(user.name);
+});
